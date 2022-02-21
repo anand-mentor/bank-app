60f815e54a4d024fcb375be60f98b539dcf84e5a
Anand Kulkarni <anand.kulkarni1@zensar.com>
2022-02-21 11:34:42 +0530

    Simple SOP added

--- TransferFundsApp/src/main/java/com/zensar/TransferFundsAppApplication.java
+++ TransferFundsApp/src/main/java/com/zensar/TransferFundsAppApplication.java
@@ -8,6 +8,7 @@ public class TransferFundsAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransferFundsAppApplication.class, args);
+		System.out.println("main exit");
 	}
 
 }

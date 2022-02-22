ca9f984fa36d3d205a547223d04636980c555c7b
Anand Kulkarni <anand.kulkarni1@zensar.com>
2022-02-22 07:32:49 +0530

    Jenkins pipeline configuration

--- TransferFundsApp/src/main/java/com/zensar/TransferFundsAppApplication.java
+++ TransferFundsApp/src/main/java/com/zensar/TransferFundsAppApplication.java
@@ -8,7 +8,6 @@ public class TransferFundsAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransferFundsAppApplication.class, args);
-		System.out.println("main exit");
 	}
 
 }
